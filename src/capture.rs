74cb9cf947209b2c2761b1776b9ff873ec04facc
LSH <github@lsh.tech>
2021-01-16 23:52:57 +0100

    wip: get eth struct from pcap data

--- src/capture.rs
+++ src/capture.rs
@@ -1,6 +1,14 @@
 use pcap::Capture;
 use std::env;
+use std::mem::transmute;
 
+
+#[repr(C)]
+struct ether {
+  ether_dhost: u8,
+  ether_shost: u8,
+  ether_type: u16,
+}
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -11,7 +19,12 @@ fn main() {
       match Capture::from_device(device_name.as_str()).unwrap().open(){
         Ok(mut cap) => {
           while let Ok(packet) = cap.next() {
-              println!("received packet! {:?}", packet);
+            unsafe {
+              let eth = transmute::<&[u8], ether>(&packet.data[0..4]);
+              println!("ether_dhost {:?}", eth.ether_dhost);
+              println!("ether_shost {:?}", eth.ether_shost);
+              println!("ether_type {:?}", eth.ether_type);
+            }
           }
         }
         Err(msg) => {
