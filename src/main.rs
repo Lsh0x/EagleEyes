a8d7f32c7ad3bdb17f6e0fb21fdc41433719d161
LSH <github@lsh.tech>
2021-01-17 23:49:48 +0100

    feat: fmt code with cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-use pcap::{Capture};
+use pcap::Capture;
 use std::env;
 
 #[repr(C, packed)]
 struct EthernetHeader {
-	dhost: [u8; 6],
-	shost: [u8; 6],
-	ether_type: u16
+    dhost: [u8; 6],
+    shost: [u8; 6],
+    ether_type: u16,
 }
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-	if args.len() != 2 {
-		println!("usage: {:?} <pcap_file>", args[0]);
-	}
-	let mut cap = Capture::from_file(args[1].as_str()).unwrap();
+    if args.len() != 2 {
+        println!("usage: {:?} <pcap_file>", args[0]);
+    }
+    let mut cap = Capture::from_file(args[1].as_str()).unwrap();
     while let Ok(packet) = cap.next() {
-      let header = unsafe {  &*(packet.data.as_ptr() as *const EthernetHeader) };
-      let t = header.ether_type.to_be();
-      match t {
-          0x0806 => println!("arp"),
-          _ => println!("unknow: {:?}", t)
-      }
+        let header = unsafe { &*(packet.data.as_ptr() as *const EthernetHeader) };
+        let t = header.ether_type.to_be();
+        match t {
+            0x0806 => println!("arp"),
+            _ => println!("unknow: {:?}", t),
+        }
     }
 }
