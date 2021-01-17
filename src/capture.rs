ed02a92e823c8e9282e71ef3fb014e07fbab3b51
Lsh0x <74725616+Lsh0x@users.noreply.github.com>
2021-01-17 16:18:43 +0100

    feat: remove mem::transmute (#1)

--- src/capture.rs
+++ src/capture.rs
@@ -1,9 +1,7 @@
 use pcap::Capture;
 use std::env;
-use std::mem::transmute;
 
 #[repr(C, packed)]
-#[derive(Debug)]
 struct EthernetHeader {
 	dhost: [u8; 6],
 	shost: [u8; 6],
@@ -19,19 +17,11 @@ fn main() {
       match Capture::from_device(device_name.as_str()).unwrap().open(){
         Ok(mut cap) => {
           while let Ok(packet) = cap.next() {
-            let mut ether_header;
-        	unsafe {
-                ether_header = std::mem::transmute::<* const u8, * const EthernetHeader>(packet.data.as_ptr()).as_ref();
-    	    }
-            match ether_header {
-                None => assert!(true, "ohhhhhhhhhhhhhhhhhhhhhh"),
-                Some(header) => {
-                    let t = header.ether_type.to_be();
-                    match t {
-                        0x0806 => println!("arp"),
-                        _ => println!("unknow: {:?}", t)
-                    }
-                }
+            let header = unsafe {  &*(packet.data.as_ptr() as *const EthernetHeader) };
+            let t = header.ether_type.to_be();
+            match t {
+                0x0806 => println!("arp"),
+                _ => println!("unknow: {:?}", t)
             }
           }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use pcap::{Device, Capture};
+use pcap::{Capture};
 use std::env;
 
 #[repr(C, packed)]
-#[derive(Debug)]
 struct EthernetHeader {
 	dhost: [u8; 6],
 	shost: [u8; 6],
@@ -17,20 +16,11 @@ fn main() {
 	}
 	let mut cap = Capture::from_file(args[1].as_str()).unwrap();
     while let Ok(packet) = cap.next() {
-        let mut ether_header;
-    	unsafe {
-            ether_header = std::mem::transmute::<* const u8, * const EthernetHeader>(packet.data.as_ptr()).as_ref();
-    	}
-        match ether_header {
-            None => assert!(true, "ohhhhhhhhhhhhhhhhhhhhhh"),
-            Some(header) => {
-                let t = header.ether_type.to_be();
-                match t {
-                    0x0806 => println!("arp"),
-                    _ => println!("unknow: {:?}", t)
-                }
-            }
-        }
-        //println!("received packet! {:?}", packet);
+      let header = unsafe {  &*(packet.data.as_ptr() as *const EthernetHeader) };
+      let t = header.ether_type.to_be();
+      match t {
+          0x0806 => println!("arp"),
+          _ => println!("unknow: {:?}", t)
+      }
     }
 }
