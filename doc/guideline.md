9411876036ae35444098d41dc6c76362c561e06f
Lsh0x <74725616+Lsh0x@users.noreply.github.com>
2021-01-19 19:06:54 +0100

    feat: pre commit cargo fmt (#3)
    * feat: fmt code with cargo fmt
    
    * feat: add pre-commit hooks for cargo fmt
    
    * feat: add documentation for dev and pre-commit

---
+++ doc/guideline.md
@@ -0,0 +1,16 @@
+## Introduction
+
+
+In order to make thing consistent, please follow the next guidelines to ensure that we are all in the same page.
+
+### Development
+
+#### Requirement
+
+To ensure that the code have the proper format, we use a `pre-commit` for formatting the code.
+Make sure you have [pre-commit](https://pre-commit.com/) installed
+
+
+#### Code
+
+We try to have the best performance possible, try to avoid copy as far as it's possible.
--- src/capture.rs
+++ src/capture.rs
@@ -3,35 +3,35 @@ use std::env;
 
 #[repr(C, packed)]
 struct EthernetHeader {
-	dhost: [u8; 6],
-	shost: [u8; 6],
-	ether_type: u16
+    dhost: [u8; 6],
+    shost: [u8; 6],
+    ether_type: u16,
 }
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-  match args.len() {
-    2 => {
-      let device_name = &args[1];
-      match Capture::from_device(device_name.as_str()).unwrap().open(){
-        Ok(mut cap) => {
-          while let Ok(packet) = cap.next() {
-            let header = unsafe {  &*(packet.data.as_ptr() as *const EthernetHeader) };
-            let t = header.ether_type.to_be();
-            match t {
-                0x0806 => println!("arp"),
-                _ => println!("unknow: {:?}", t)
+    match args.len() {
+        2 => {
+            let device_name = &args[1];
+            match Capture::from_device(device_name.as_str()).unwrap().open() {
+                Ok(mut cap) => {
+                    while let Ok(packet) = cap.next() {
+                        let header = unsafe { &*(packet.data.as_ptr() as *const EthernetHeader) };
+                        let t = header.ether_type.to_be();
+                        match t {
+                            0x0806 => println!("arp"),
+                            _ => println!("unknow: {:?}", t),
+                        }
+                    }
+                }
+                Err(msg) => {
+                    println!("error: {:?}", msg);
+                }
             }
-          }
         }
-        Err(msg) => {
-        	println!("error: {:?}", msg);
+        _ => {
+            println!("usage: <device name>")
         }
-      }
     }
-    _ => {
-      println!("usage: <device name>")
-    }
-  }
 }
