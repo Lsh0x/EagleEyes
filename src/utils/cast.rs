244ea8c05fbf17163041de9a7a1bccc88e8f35c4
alexandre merle <alexandre.merle@scality.com>
2021-01-23 21:31:38 +0100

    safe casting

--- src/utils/cast.rs
+++ src/utils/cast.rs
@@ -1,3 +1,7 @@
-pub fn cast_slice_to_reference<T>(data: &[u8]) -> &T {
-    unsafe { &*(data.as_ptr() as *const T) }
+pub fn cast_slice_to_reference<T>(data: &[u8]) -> std::result::Result::<&T, &str> {
+	if data.len() < std::mem::size_of::<T>() {
+		Err("Truncated payload")
+	} else {
+		Ok(unsafe { &*(data.as_ptr() as *const T) })
+	}   
 }
