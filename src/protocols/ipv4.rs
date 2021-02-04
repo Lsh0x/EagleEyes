269cbca17ed6a992d0b85558fb6dc8f57db0825c
LSH <github@lsh.tech>
2021-02-05 00:05:00 +0100

    feat: call esp decode and cleanup

--- src/protocols/ipv4.rs
+++ src/protocols/ipv4.rs
@@ -1,11 +1,13 @@
 use crate::utils::cow_struct;
 use std::mem::size_of;
 
+use super::esp;
 use super::ip;
 
 #[derive(Default, Debug, Clone, Copy)]
 #[repr(C, packed)]
 pub struct IPV4Header {
+    /// version of the ip protocol and the lenght of the header since it can be variable du to options
     pub version_and_header_len: u8,
     pub type_of_service: u8,
     pub total_len: u16,
@@ -24,16 +26,19 @@ impl IPV4Header {
 
 pub fn decode(data: &[u8]) {
     if data.len() >= IPV4Header::SIZE {
-        let (slice, _data) = data.split_at(IPV4Header::SIZE);
-        match cow_struct::<IPV4Header>(slice) {
+        let (header_bytes, _next_data) = data.split_at(IPV4Header::SIZE);
+        match cow_struct::<IPV4Header>(header_bytes) {
             Some(header) => {
                 let version = (header.version_and_header_len & 0xF0) >> 4;
                 if version != 4 {
                     println!("Invalid ip version: {:?}", version);
                 } else {
+                    // ipv4 header with potential options included
                     let len_bytes: usize = ((header.version_and_header_len & 0xF) * 32 / 8).into();
-                    let _current_data = &data[len_bytes..];
-                    println!("protocol {:?}", ip::protocol_as_str(header.protocol));
+                    match header.protocol {
+                        ip::PROTO::ESP => esp::decode(&data[len_bytes..]),
+                        _ => println!("protocol::ipv4 {:?}", ip::protocol_as_str(header.protocol)),
+                    }
                 }
             }
             None => println!("ip decode error: {:?}", "Truncated payload"),
